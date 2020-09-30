84a05a421beacf0965b71f934c74bf9bca4c4089
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-30 18:41:14 -0300

    Listando categorias e comidas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -69,14 +69,17 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get('categories');
+
+      setCategories(response.data);
     }
 
     loadCategories();
   }, []);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    const selectedCategoryId = id === selectedCategory ? undefined : id;
+    setSelectedCategory(selectedCategoryId);
   }
 
   return (
